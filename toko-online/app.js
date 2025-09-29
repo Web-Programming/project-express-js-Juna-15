55b6a2fa22ce994a2ce1ba6f83ac237aec586186
Juna-15 <muhammadjndi@gmail.com>
2025-09-29 17:00:35 +0700

    pertemuan6

--- toko-online/app.js
+++ toko-online/app.js
@@ -4,13 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var engine = require('ejs-blocks');
+//perbaikan ke 2
+var indexRouter = require('./app_toko_online/routes/index');
+var usersRouter = require('./app_toko_online/routes/users');
+var productRouter = require("./app_toko_online/routes/product");
+var engine = require('ejs-blocks');//menggunakan ejs block
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_toko_online', 'views'));//perbaikan 1
 app.engine('ejs',engine);
 app.set('view engine', 'ejs');
 
@@ -25,7 +27,6 @@ app.use('/bootstrap',express.static(path.join(__dirname,'node_modules/bootstrap/
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-var productRouter = require("./routes/product");
 app.use("/product",productRouter);
 
 // catch 404 and forward to error handler
---
+++ toko-online/app_toko_online/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var mainController = require("../controllers/main");
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'toko online sederhada',products:products });
+// });
+
+// router.get('/search', function(req, res, next) {
+//   const q = req.query.q ? req.query.q.toLowerCase() : ""; // ambil query, default ""
+  
+//   let filteredProducts = products;
+
+//   if (q) {
+//     filteredProducts = products.filter(p => 
+//       p.name.toLowerCase().includes(q)
+//     );
+//   }
+
+//   res.render('index', { 
+//     title: q ? `Hasil Pencarian untuk "${req.query.q}"` : 'Toko Online Sederhana',
+//     products: filteredProducts,
+//     query: req.query.q || ""
+//   });
+// });
+
+router.get("/",mainController.index);
+module.exports = router;
