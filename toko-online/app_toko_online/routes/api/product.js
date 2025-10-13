f7210b477b780cd2e8c45035fa0e5aef66ec10aa
Juna-15 <muhammadjndi@gmail.com>
2025-10-13 17:05:35 +0700

    pertemuan10

---
+++ toko-online/app_toko_online/routes/api/product.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+var productController = require("../controllers/product");
+
+router.get("/apiall", productController.all);
+router.get("/all", productController.index);
+router.get("/:id", productController.detail);
+
+
+module.exports = router;
